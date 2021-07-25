61c7f8c729e4ce2f996ce91e3d40d44a554829e8
Tejeet <Tejeetm@gmail.com>
2021-07-25 13:00:57 +0530

    App Initialized With Splash Screen and All Dependencies

---
+++ SaveoDemoApp/app/src/main/java/com/tejeet/saveodemoapp/constants/ConstantsData.kt
@@ -0,0 +1,13 @@
+package com.tejeet.nobrokerdemoapi.constants
+
+object ConstantsData {
+
+    const val SPLASH_SCREEN_TIMEOUT : Long = 1000;
+
+    const val DATA_KEY = "DATA";
+
+
+    const val API_BASE_URL = "https://api.tvmaze.com/"
+    const val SEARCH_END_POINT = "search/shows?q=friction"
+
+}
---
+++ SaveoDemoApp/app/src/main/java/com/tejeet/saveodemoapp/ui/SplashScreen.kt
@@ -0,0 +1,31 @@
+package com.tejeet.saveodemoapp.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tejeet.nobrokerdemoapi.constants.ConstantsData
+import com.tejeet.saveodemoapp.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(ConstantsData.SPLASH_SCREEN_TIMEOUT);  // Splash Screen Wait time Defined in Constant File
+            gotoDashboard();
+        }
+
+    }
+
+    private fun gotoDashboard() {
+        intent = Intent(this, MainActivity::class.java);
+        startActivity(intent);
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+        finish();
+    }
+}
